4c631213f235c3f3fddb34294ec740e3ba39303f
Eric Wong <ericwong12@live.com>
2020-03-02 19:24:13 -0700

    Updated session (secret)

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(express.static("public"));
 // We need to use sessions to keep track of our user's login status
 app.use(
-  session({ secret: "keyboard cat", resave: true, saveUninitialized: true })
+  session({ secret: "Laughing vanilla", resave: true, saveUninitialized: true })
 );
 app.use(passport.initialize());
 app.use(passport.session());
