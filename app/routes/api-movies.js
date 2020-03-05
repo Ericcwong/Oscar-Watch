90ba8ef451ac8ebfa096acd98e023b953bd4f47c
Dan D <degeare28@gmail.com>
2020-03-04 21:16:45 -0700

    changed models to export and added associations

---
+++ app/routes/api-movies.js
@@ -0,0 +1,102 @@
+// *********************************************************************************
+// api-routes.js - this file offers a set of routes for displaying and saving data to the db
+// *********************************************************************************
+
+// Dependencies
+// =============================================================
+
+// Requiring our Todo model
+var db = require("../models");
+
+// Routes
+// =============================================================
+module.exports = function(app) {
+  // GET route for getting all of the movies
+  app.get("/api/movies/", function(req, res) {
+    db.Movies.findAll({}).then(function(result) {
+      res.json(result);
+    });
+  });
+
+  // Get route for returning awards of a specific category
+  app.get("/api/movies/category/:category", function(req, res) {
+    db.Movies.findAll({
+      where: {
+        catagories: req.params.category
+      }
+    }).then(function(result) {
+      res.json(result);
+    });
+  });
+
+  // Get route for returning a specific Movie
+  app.get("/api/movies/:movie", function(req, res) {
+    db.Movies.findAll({
+      where: {
+        movieName: req.params.movie
+      }
+    }).then(function(result) {
+      res.json(result);
+    });
+  });
+
+  // Get route for returning winners of a specific name
+  app.get("/api/movies/name/:name", function(req, res) {
+    db.Movies.findAll({
+      where: {
+        name: req.params.name
+      }
+    }).then(function(result) {
+      res.json(result);
+    });
+  });
+
+  // Get route for returning winners of a specific year
+  app.get("/api/movies/year/:year", function(req, res) {
+    db.Movies.findAll({
+      where: {
+        awardYear: req.params.year
+      }
+    }).then(function(result) {
+      res.json(result);
+    });
+  });
+
+  // POST route for saving a new Movie
+  app.post("/api/movies", function(req, res) {
+    console.log(req.body);
+    db.Movies.create({
+      filmYear: req.body.filmYear,
+      awardYear: req.body.awardYear,
+      ceremony: req.body.ceremony,
+      catagories: req.body.body,
+      name: req.body.category,
+      movieName: req.body.title,
+      isWinner: req.body.isWinner
+    }).then(function(result) {
+      res.json(result);
+    });
+  });
+
+  // DELETE route for deleting posts
+  app.delete("/api/movies/:id", function(req, res) {
+    db.Movies.destroy({
+      where: {
+        id: req.params.id
+      }
+    }).then(function(result) {
+      res.json(result);
+    });
+  });
+
+  // PUT route for updating posts
+  app.put("/api/posts", function(req, res) {
+    db.Movies.update(req.body, {
+      where: {
+        id: req.body.id
+      }
+    }).then(function(result) {
+      res.json(result);
+    });
+  });
+};
