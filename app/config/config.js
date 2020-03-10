54fc6c257b810335741aeca5443e0405faa2bbec
Dan D <degeare28@gmail.com>
2020-03-09 18:27:23 -0600

    changed config to reference password

--- app/config/config.js
+++ app/config/config.js
@@ -1,27 +1,28 @@
+/* eslint-disable camelcase */
 module.exports = {
-  "development": {
-    "username": process.env.mySQL_username,
-    "password": process.env.mySQL_username,
-    "database": "Oscar_watch_db",
-    "host": "127.0.0.1",
-    "dialect": "mysql",
-    "operatorsAliases": 1
+  development: {
+    username: process.env.mySQL_username,
+    password: process.env.mySQL_password,
+    database: "Oscar_watch_db",
+    host: "127.0.0.1",
+    dialect: "mysql",
+    operatorsAliases: 1
   },
-  "test": {
-    "username": process.env.mySQL_username,
-    "password": process.env.mySQL_username,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "mysql",
-    "operatorsAliases": false
+  test: {
+    username: process.env.mySQL_username,
+    password: process.env.mySQL_password,
+    database: "database_test",
+    host: "127.0.0.1",
+    dialect: "mysql",
+    operatorsAliases: false
   },
-  "production": {
-    "use_env_variable": "JAWSDB_URL",
-    "username": process.env.mySQL_username,
-    "password": process.env.mySQL_username,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql",
-    "operatorsAliases": false
+  production: {
+    use_env_variable: "JAWSDB_URL",
+    username: process.env.mySQL_username,
+    password: process.env.mySQL_password,
+    database: "database_production",
+    host: "127.0.0.1",
+    dialect: "mysql",
+    operatorsAliases: false
   }
 };
