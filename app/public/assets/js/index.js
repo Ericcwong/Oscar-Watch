c8e7fdb667fe8701069f97f275d08c5c7b309831
Dalton Earl <dalton.earl@outlook.com>
2020-03-09 20:36:28 -0600

    The drop down is getting the correct string to send to the database

--- app/public/assets/js/index.js
+++ app/public/assets/js/index.js
@@ -2,17 +2,18 @@
 //   console.log(data);
 // });
 $(document).ready(function (){
-  const category = $("#oscar_category :selected").text();
-  const nomineeName = $("#nominee_name");
-  const nominatedFilm = $("#film_nominated");
-  const filmYear = $("#film_production_year");
-  const winner = $("#is_winner");
-  const searchResults = $("#search_results");
-  const searchForm = $("#search-movies");
+  let category = $("#oscar_category :selected").text();
+  let nomineeName = $("#nominee_name");
+  let nominatedFilm = $("#film_nominated");
+  let filmYear = $("#film_production_year");
+  let winner = $("#is_winner");
+  let searchResults = $("#search_results");
+  let searchForm = $("#search-movies");
 
-  function categorySearch(category){
+  function categorySearch(){
+    let selectedCategory = $("#oscar_category :selected").text();
     console.log("Line 19");
-    $.get(`/api/movies/category/${category}`, function(data){
+    $.get(`/api/movies/category/${selectedCategory}`, function(data){
       console.log(data);
 
     });
