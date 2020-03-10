07c20e304be11d672605b8ef3860f27dcd2711a2
Eric Wong <ericwong12@live.com>
2020-03-09 18:39:45 -0600

    File pull update

--- app/config/config.js
+++ app/config/config.js
@@ -1,32 +1,4 @@
 module.exports = {
-<<<<<<< HEAD
-    "development": {
-      "username": process.env.mySQL_username,
-      "password": process.env.mySQL_username,
-      "database": "Oscar_watch_db",
-      "host": "127.0.0.1",
-      "dialect": "mysql",
-      "operatorsAliases": 1
-    },
-    "test": {
-      "username": process.env.mySQL_username,
-      "password": process.env.mySQL_username,
-      "database": "database_test",
-      "host": "127.0.0.1",
-      "dialect": "mysql",
-      "operatorsAliases": false
-    },
-    "production": {
-      "use_env_variable": "JAWSDB_URL",
-      "username": process.env.mySQL_username,
-      "password": process.env.mySQL_username,
-      "database": "database_production",
-      "host": "127.0.0.1",
-      "dialect": "mysql",
-      "operatorsAliases": false
-    }
-  };
-=======
   "development": {
     "username": process.env.mySQL_username,
     "password": process.env.mySQL_username,
@@ -53,4 +25,3 @@ module.exports = {
     "operatorsAliases": false
   }
 };
->>>>>>> master
