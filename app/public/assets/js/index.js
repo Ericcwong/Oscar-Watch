dcc33a792ade4a8bf0f2ed0c602536c238a34df4
Dalton Earl <dalton.earl@outlook.com>
2020-03-09 19:50:08 -0600

    The catagory search drop down is sending a string to the Database

--- app/public/assets/js/index.js
+++ app/public/assets/js/index.js
@@ -1,3 +1,46 @@
-d3.csv("../../database/Copy of the_oscar_award.csv").then(function(data){
-  console.log(data);
+// d3.csv("../../database/Copy of the_oscar_award.csv").then(function(data){
+//   console.log(data);
+// });
+$(document).ready(function (){
+  const category = $("#oscar_category :selected").text();
+  const nomineeName = $("#nominee_name");
+  const nominatedFilm = $("#film_nominated");
+  const filmYear = $("#film_production_year");
+  const winner = $("#is_winner");
+  const searchResults = $("#search_results");
+  const searchForm = $("#search-movies");
+
+  function categorySearch(category){
+    console.log("Line 19");
+    $.get(`/api/movies/category/${category}`, function(data){
+      console.log(data);
+
+    });
+
+  }
+  console.log("line 26");
+
+  function movieSearch(){
+
+  }
+  function nameSearch(){
+
+  }
+  function yearSearch(){
+
+  }
+  function displaySearch(){
+
+  }
+  function searchOscars(event){
+    event.preventDefault();
+    categorySearch(category);
+
+
+  }
+
+
+
+  searchForm.on("submit",searchOscars);
 });
+
