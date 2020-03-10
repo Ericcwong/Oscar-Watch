ca15be16e1ad784ec9700e18b23f6827adb4652f
Eric Wong <ericwong12@live.com>
2020-03-09 18:44:07 -0600

    adding changes

--- app/models/index.js
+++ app/models/index.js
@@ -6,11 +6,7 @@ var path = require("path");
 var Sequelize = require("sequelize");
 var basename = path.basename(module.filename);
 var env = process.env.NODE_ENV || "development";
-<<<<<<< HEAD
-var config = require(__dirname + "/../config/config.json")[env];
-=======
 var config = require(__dirname + "/../config/config.js")[env];
->>>>>>> master
 var db = {};
 
 if (config.use_env_variable) {
