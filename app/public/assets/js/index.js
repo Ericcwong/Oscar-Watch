5e7442b1d8e4986616d378bb52742d225cf8f42b
Eric Wong <ericwong12@live.com>
2020-03-11 19:15:56 -0600

    Movie cards is working but still needs more function

--- app/public/assets/js/index.js
+++ app/public/assets/js/index.js
@@ -25,32 +25,58 @@ $(document).ready(function() {
     //Takes in user input from DOM
     let enteredMovie = $("#film_nominated").val();
     //This is suppose to display the movie poster but is not aligning how it is supposed to be
-    // let image = $.ajax({
-    //   url: `http://www.omdbapi.com/?t=${enteredMovie}&apikey=bdaebc3a`,
-    //   method: "GET"
-    // }).then(function(res){
-    //   console.log(res.Poster);
-    // });
-    // Sets obj to pull in infomation from the database based on what the user entered from the DOM
-    let obj = $.get(`/api/movies/${enteredMovie}`, function(data) {
-      console.log(enteredMovie);
-      console.log(data);
-      for (i = 0; i < data.length; i++) {
-        //Create the cards
-        let card = `<div class="card-header bg-primary">
-          <h2 class="text-white">${data[i].movieName}</h2>
-          <img src = "" alt = "Movie Poster">
-          <h3 class="text-white">${data[i].filmYear}</h3></div>
-          <h3 class="text-white">${data[i].awardYear}</h3></div>
-          <div class="card-body">
-            <p class="card-text"><strong>Catagories: </strong>${data[i].catagories}</p>
-            <p class="card-text"><strong>Name: </strong>${data[i].name}</p>
-          </div>
-        </div>`;
-        //appends the cards to the row search_results
-        $("#search_results").append(card);
-      }
+    let image;
+    $.ajax({
+      url: `http://www.omdbapi.com/?t=${enteredMovie}&apikey=bdaebc3a`,
+      method: "GET"
+    }).then(function(res){
+      console.log(res.Poster);
+      image = res.Poster;
+      $.get(`/api/movies/${enteredMovie}`, function(data) {
+        console.log(enteredMovie);
+        console.log(data);
+        for (i = 0; i < data.length; i++) {
+          //Create the cards
+          let card = `<div class="card-header bg-primary">
+            <h2 class="text-white">${data[i].movieName}</h2>
+            <img src = "${image}" alt = "Movie Poster">
+            <h3 class="text-white">${data[i].filmYear}</h3></div>
+            <h3 class="text-white">${data[i].awardYear}</h3></div>
+            <div class="card-body">
+              <p class="card-text"><strong>Catagories: </strong>${data[i].catagories}</p>
+              <p class="card-text"><strong>Name: </strong>${data[i].name}</p>
+            </div>
+          </div>`;
+          //appends the cards to the row search_results
+          $("#search_results").append(card);
+        }
+      });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      
     });
+    // Sets obj to pull in infomation from the database based on what the user entered from the DOM
+
 
     // $("#search_results").append(test);
   }
